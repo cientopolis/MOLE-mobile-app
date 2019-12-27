555659ba41310d1233da0867acb4af267a406326
Federico Mozzon <mozzonfederico@gmail.com>
2019-12-27 12:49:24 -0300

    Api address

--- src/helpers/apiConnection.js
+++ src/helpers/apiConnection.js
@@ -1,6 +1,6 @@
 import checkFormat from './checkActivityFormat'
 
-const API = 'http://192.168.10.154:3001/api/'
+const API = 'http://api-mole.lifia.info.unlp.edu.ar/api'
 
 const getActivity = id => fetch(API + `Activities/${id}`).then(
   result => result.json().then(
